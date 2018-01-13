ae6674565adb3bea15053af62125e330d559876e
lelyonnais78 <35397695+lelyonnais78@users.noreply.github.com>
2018-01-13 12:50:47 +0100

    Initial commit

---
+++ data/LINESTRAJET0.js
@@ -0,0 +1,495 @@
+var json_LINESTRAJET0 ={  
+   "type":"FeatureCollection",
+   "name":"LINESTRAJET0",
+   "crs":{  
+      "type":"name",
+      "properties":{  
+         "name":"urn:ogc:def:crs:OGC:1.3:CRS84"
+      }
+   },
+   "features":[  
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"1",
+            "POINT B":"2",
+            "DEPART":"NANTES",
+            "ARRIVEE":"RIO",
+            "DUREE":"1 semaine"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -1.553621,
+                  47.218371
+               ],
+               [  
+                  -43.172896,
+                  -22.906847
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"2",
+            "POINT B":"3",
+            "DEPART":"RIO",
+            "ARRIVEE":"BELO HORIZONTE",
+            "DUREE":"2 j"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -43.172896,
+                  -22.906847
+               ],
+               [  
+                  -43.935238,
+                  -19.924502
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"3",
+            "POINT B":"4",
+            "DEPART":"BELO HORIZONTE",
+            "ARRIVEE":"OURO PRETO",
+            "DUREE":"2 j"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -43.935238,
+                  -19.924502
+               ],
+               [  
+                  -43.503578,
+                  -20.385574
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"4",
+            "POINT B":"5",
+            "DEPART":"OURO PRETO",
+            "ARRIVEE":"SALVADOR DE BAHIA",
+            "DUREE":"10 j"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -43.503578,
+                  -20.385574
+               ],
+               [  
+                  -38.501414,
+                  -12.972218
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"5",
+            "POINT B":"2",
+            "DEPART":"SALVADOR DE BAHIA",
+            "ARRIVEE":"RIO",
+            "DUREE":"10 j"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -38.501414,
+                  -12.972218
+               ],
+               [  
+                  -43.172896,
+                  -22.906847
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"4",
+            "POINT B":"6",
+            "DEPART":"RIO",
+            "ARRIVEE":"CURITIBA",
+            "DUREE":"2 j"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                 -43.172896,
+                  -22.906847
+               ],
+               [  
+                  -49.265382,
+                  -25.424429
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"6",
+            "POINT B":"7",
+            "DEPART":"CURITIBA",
+            "ARRIVEE":"PUERTO IGUAZU",
+            "DUREE":"1 semaine"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -49.265382,
+                  -25.424429
+               ],
+               [  
+                  -54.578599,
+                  -25.597164
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"7",
+            "POINT B":"8",
+            "DEPART":"PUERTO IGUAZU",
+            "ARRIVEE":"SALTA",
+            "DUREE":"1 semaine"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -54.578599,
+                  -25.597164
+               ],
+               [  
+                  -65.423198,
+                  -24.782127
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"8",
+            "POINT B":"9",
+            "DEPART":"SALTA",
+            "ARRIVEE":"SAN PEDRO DE ATACAMA",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -65.423198,
+                  -24.782127
+               ],
+               [  
+                  -68.199716,
+                  -22.908707
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"9",
+            "POINT B":"10",
+            "DEPART":"SAN PEDRO DE ATACAMA",
+            "ARRIVEE":"UYUNI",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -68.199716,
+                  -22.908707
+               ],
+               [  
+                  -67.489134,
+                  -20.133777
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"10",
+            "POINT B":"11",
+            "DEPART":"UYUNI",
+            "ARRIVEE":"LA PAZ",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -67.489134,
+                  -20.133777
+               ],
+               [  
+                  -68.119294,
+                  -16.489689
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"11",
+            "POINT B":"12",
+            "DEPART":"LA PAZ",
+            "ARRIVEE":"IQUIQUE",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -68.119294,
+                  -16.489689
+               ],
+               [  
+                  -70.135669,
+                  -20.230703
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"12",
+            "POINT B":"13",
+            "DEPART":"IQUIQUE",
+            "ARRIVEE":"SANTIAGO DE CHILE",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -70.135669,
+                  -20.230703
+               ],
+               [  
+                  -70.669265,
+                  -33.44889
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"13",
+            "POINT B":"14",
+            "DEPART":"SANTIAGO DE CHILE",
+            "ARRIVEE":"VALPARAISO",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -70.669265,
+                  -33.44889
+               ],
+               [  
+                  -71.612688,
+                  -33.047238
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"14",
+            "POINT B":"15",
+            "DEPART":"VALPARAISO",
+            "ARRIVEE":"BARILOCHE",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -71.612688,
+                  -33.047238
+               ],
+               [  
+                  -71.310278,
+                  -41.133472
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"15",
+            "POINT B":"16",
+            "DEPART":"BARILOCHE",
+            "ARRIVEE":"PERITO MORENO",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -71.310278,
+                  -41.133472
+               ],
+               [  
+                  -73.137661,
+                  -50.49673
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"16",
+            "POINT B":"17",
+            "DEPART":"PERITO MORENO",
+            "ARRIVEE":"USHUAIA",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -73.137661,
+                  -50.49673
+               ],
+               [  
+                  -68.302951,
+                  -54.801912
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"17",
+            "POINT B":"18",
+            "DEPART":"USHUAIA",
+            "ARRIVEE":"BUENOS AIRES",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -68.302951,
+                  -54.801912
+               ],
+               [  
+                  -58.381559,
+                  -34.603684
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"18",
+            "POINT B":"19",
+            "DEPART":"BUENOS AIRES",
+            "ARRIVEE":"LIMA",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -58.381559,
+                  -34.603684
+               ],
+               [  
+                  -77.042754,
+                  -12.046373
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"19",
+            "POINT B":"20",
+            "DEPART":"LIMA",
+            "ARRIVEE":"MEDELIN",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -77.042754,
+                  -12.046373
+               ],
+               [  
+                  -75.581212,
+                  6.244203
+               ]
+            ]
+         }
+      },
+      {  
+         "type":"Feature",
+         "properties":{  
+            "POINT A":"20",
+            "POINT B":"21",
+            "DEPART":"MEDELIN",
+            "ARRIVEE":"SAN JOSE",
+            "DUREE":"On verra"
+         },
+         "geometry":{  
+            "type":"LineString",
+            "coordinates":[  
+               [  
+                  -75.581212,
+                  6.244203
+               ],
+               [  
+                  -84.090725,
+                  9.928069
+               ]
+            ]
+         }
+      }
+   ]
+}
